026818f77907f23834ae51746ec35d369ca0b2ef
saintsofearth <apoorvarjaria24@gmail.com>
2020-07-23 21:58:23 +0530

    Section 9: Building the search controller.

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -5,11 +5,11 @@ export default class Search {
         this.query = query;
     }
 
-    async getRecipes() {
+    async getResults() {
         try {
             const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
             this.result = res.data.recipes;
-            console.log(this.result);
+            // console.log(this.result);
         } catch (error) {
             alert(error);
         }
